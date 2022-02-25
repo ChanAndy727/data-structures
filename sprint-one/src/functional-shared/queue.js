07fe1188444b9dfcd28c94f63d4428d02e69fa57
Andy Chan <Chanandy727@gmail.com>
2022-02-25 01:27:24 -0800

    passed all functionally shared implementations tests for queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(string) {
+  this.storage[Object.keys(this.storage).length] = string;
+};
 
+queueMethods.dequeue = function() {
+  var top;
+  if (this.storage['0']) {
+    top = this.storage['0'];
+    delete this.storage['0'];
+    for (let key in this.storage) {
+      var newKey = parseInt(key) - 1;
+      this.storage[newKey] = this.storage[key];
+      delete this.storage[key];
+    }
+  }
+  return top;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
