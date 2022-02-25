f49007884760697ba1ba93f365921cfc4d04a617
Andy Chan <Chanandy727@gmail.com>
2022-02-24 18:52:11 -0800

    failing functional size implementation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,30 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if (storage['0']) {
+      for (let key in storage) {
+        var newKey = parseInt(key) + 1;
+        storage[newKey] = storage[key];
+      }
+    }
+    storage['0'] = value;
+
   };
 
   someInstance.pop = function() {
+    var top;
+    if (storage['0']) {
+      top = storage[0];
+      for (let key in storage) {
+        var newKey = parseInt(key) - 1;
+        storage[newKey] = storage[key];
+      }
+    }
+    return top;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
