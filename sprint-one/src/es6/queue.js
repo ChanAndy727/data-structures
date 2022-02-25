3947f8399a1000cee76a6826661af78357f75691
Andy Chan <Chanandy727@gmail.com>
2022-02-25 13:08:12 -0800

    Passed all tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
+
+  enqueue(string) {
+    this.storage[Object.keys(this.storage).length] = string;
+  }
+
+  dequeue() {
+    var top;
+    if (this.storage['0']) {
+      top = this.storage['0'];
+      delete this.storage['0'];
+      for (let key in this.storage) {
+        var newKey = parseInt(key) - 1;
+        this.storage[newKey] = this.storage[key];
+        delete this.storage[key];
+      }
+    }
+    return top;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,35 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(string) {
+    if (this.storage['0']) {
+      for (let key in this.storage) {
+        var newKey = parseInt(key) + 1;
+        this.storage[newKey] = this.storage[key];
+      }
+    }
+    this.storage['0'] = string;
+  }
+
+  pop() {
+    var top;
+    if (this.storage['0']) {
+      top = this.storage['0'];
+      delete this.storage['0'];
+      for (let key in this.storage) {
+        var newKey = parseInt(key) - 1;
+        this.storage[newKey] = this.storage[key];
+        delete this.storage[key];
+      }
+    }
+    return top;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
