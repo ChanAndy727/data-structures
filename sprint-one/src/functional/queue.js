24f8946c34a574ce4508129ad0541ee01ff11ae4
Andy Chan <Chanandy727@gmail.com>
2022-02-24 18:51:10 -0800

    failing size relationship test on functional implementation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.dequeue = function() {
+    var value;
+    if (storage['0']) {
+      value = storage['0'];
+      delete storage['0'];
+      for (let key in storage) {
+        var newKey = (parseInt(key) - 1);
+        storage[newKey] = storage[key];
+      }
+    }
+    return value;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
